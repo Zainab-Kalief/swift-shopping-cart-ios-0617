22166e50bc8ce233c78ac2dc991be4fb40d0c386
Zainab-Kalief <alesewura@yahoo.com>
2017-06-16 09:59:09 -0700

    Shopping Cart

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -7,6 +7,60 @@
 //
 
 class Cart {
+    var items: [Item] = []
     
+    func totalPriceInCents() -> Int {
+        var total = 0
+        
+        for item in items {
+            total += item.priceInCents
+        }
+        return total
+    }
+    
+    func remove(item: Item) {
+        for (index, value) in items.enumerated() {
+            if value == item {
+                items.remove(at: index)
+            }
+        }
+    }
+    
+    func add(item: Item) {
+        self.items.append(item)
+    }
+    
+    func items(withName name: String) -> [Item] {
+        var newList: [Item] = []
+        
+        for item in self.items {
+            if item.name == name {
+                newList.append(item)
+            }
+        }
+        return newList
+    }
+    
+    func items(withMinPrice price: Int) -> [Item] {
+        var newList: [Item] = []
+        
+        for item in items {
+            if item.priceInCents >= price {
+                newList.append(item)
+            }
+        }
+        return newList
+    }
+    
+    func items(withMaxPrice price: Int) -> [Item] {
+        var newList: [Item] = []
+        
+        for item in items {
+            if item.priceInCents <= price {
+                newList.append(item)
+            }
+        }
+        return newList
+    }
     
 }
